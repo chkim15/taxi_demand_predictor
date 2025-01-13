02e1d4df0c874859b2bdb97ab2134c1e83421e1a
Chulhee Kim <chulhee.kim15@gmail.com>
2025-01-13 11:23:01 -0500

    Added feature pipeline and github actions

--- src/config.py
+++ src/config.py
@@ -19,7 +19,8 @@ except:
 
 # TODO: remove FEATURE_GROUP_NAME and FEATURE_GROUP_VERSION, and use FEATURE_GROUP_METADATA instead
 FEATURE_GROUP_NAME = 'time_series_hourly_feature_group'
-FEATURE_GROUP_VERSION = 3
+FEATURE_GROUP_VERSION = 2
+
 # FEATURE_GROUP_METADATA = FeatureGroupConfig(
 #     name='time_series_hourly_feature_group',
 #     version=3,
